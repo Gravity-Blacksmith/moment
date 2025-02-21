4062c3c88ac7e30df32cf922c5f9669387329ae7
Emilie L'Haridon <emilie.lharidon@gmail.com>
2025-02-21 15:31:31 +0100

    feat: SEO information

--- pages/index.vue
+++ pages/index.vue
@@ -398,6 +398,33 @@ const { t } = useI18n();
 
 import { onMounted } from "vue";
 import { gsap } from "gsap";
+const { locale } = useI18n();
+
+const metaConfig = {
+  en: {
+    description:
+      "The first SAAS platform that simplifies and accelerates ESG assessments for both buyers and suppliers using artificial intelligence.",
+    twitterCard: "summary_large_image",
+  },
+  fr: {
+    description:
+      "La première plateforme SAAS qui simplifie et accélère les évaluations ESG pour les acheteurs et les fournisseurs en utilisant l'intelligence artificielle.",
+    twitterCard: "summary_large_image",
+  },
+};
+
+useSeoMeta({
+  title: "Moment.green",
+  ogTitle: "Moment.green",
+  description: () =>
+    metaConfig[locale.value]?.description ||
+    "La première plateforme SAAS qui simplifie et accélère les évaluations ESG pour les acheteurs et les fournisseurs en utilisant l'intelligence artificielle.'",
+  ogDescription: () =>
+    metaConfig[locale.value]?.description ||
+    "La première plateforme SAAS qui simplifie et accélère les évaluations ESG pour les acheteurs et les fournisseurs en utilisant l'intelligence artificielle.'",
+  ogImage: "/public/moment_image.png",
+  twitterCard: "summary_large_image",
+});
 
 onMounted(() => {
   gsap.to("#content", { opacity: 1, duration: 1 });
