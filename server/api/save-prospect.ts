21c9e72850a3c1ea3784b896fc38a46388e9542d
Emilie L'Haridon <emilie.lharidon@gmail.com>
2025-03-25 17:29:10 +0100

    fix: test insert

--- server/api/save-prospect.ts
+++ server/api/save-prospect.ts
@@ -20,18 +20,17 @@ export default defineEventHandler(async (event) => {
   const sheets = google.sheets({ version: 'v4', auth })
   const spreadsheetId = config.googleSpreadsheetId;
   console.log('Google sheets found', spreadsheetId);
-  const range = 'Feuille 1!A';
+  const range = 'Feuille 1!A:A';
 
   const { name, email } = body;
   console.log('Name and email', name, email);
 
-  console.log(sheets.spreadsheets.values);
   const doc = await sheets.spreadsheets.values.append({
     spreadsheetId,
     range,
     valueInputOption: 'RAW',
     insertDataOption: 'INSERT_ROWS',
-    requestBody: { values: [name, email, new Date().toISOString()] }
+    requestBody: { values: [[name, email, new Date().toISOString()]] }
   })
   console.log('Doc', doc);
   return { success: true }
